01a47df51a22b12e7bb7ef5653b6cfb26bc10ffd
dalhun namgung <bnamgung@gmail.com>
2017-10-23 14:54:23 -0700

    added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 'use strict';
 
-const List = require('./lib/List');
+const list =
+require('./lib/list.js');
+require('./lib/prepend.js');
+require('./lib/append.js');
+require('./lib/reverse.js');
+require('./lib/remove.js');
+
+let listOne = new list();
+
+listOne.prepend(2);
+listOne.prepend(3);
+listOne.prepend(1);
+listOne.reverse();
+listOne.print();
