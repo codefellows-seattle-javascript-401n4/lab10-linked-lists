b8d902abd38b9e8cbd7bc63c0a62bd3a02f65d5e
meganr <meganrm@gmail.com>
2017-10-24 16:36:41 -0700

    added remove

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 'use strict';
 
-const LL = require ('./lib/LinkedList')
+const LL = require ('./lib/LinkedList');
 
-let ll1= new LL();
+let ll1 = new LL();
 
-ll1.append('first');
-ll1.append('next');
+ll1.append('zero');
+ll1.append('one');
+ll1.append('two');
+ll1.append('three');
+ll1.append('four');
+ll1.append('five');
 
-console.log(ll1);
+
+// console.log(ll1);
+// ll1.print()
+ll1.reverse()
+ll1.print()
