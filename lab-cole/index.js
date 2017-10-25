36bc3319a4ccb519badc3abce0633051858efca9
Cole Allen <colesss>
2017-10-24 17:57:23 -0700

    added scaffolding and have started implementing some code but will need to resubmit

---
+++ lab-cole/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const util = require('util');
+const LL = require('./lib/ll.js');
+
+//append(value)
+
+//prepend(value)
+
+//reverse()
+
+//remove(offset)
---
+++ lab-cole/lib/ll.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Node = require('./lib/node.js');
+const LinkedList = module.exports = node => {
+    this.head = node || null;
+};
+
+LinkedList.prototype.append(value)
+
+LinkedList.prototype.prepend = function (val) {
+    let oldHead = this.head;
+    this.headd = Node ||null;
+};
+
+LinkedList.prototype.reverse()
+
+LinkedList.prototype.remove(offset)
