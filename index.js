06ccc7c3983c8771c1d47a243f997b70918dd2b2
Darryl Davis <ddavis1306@gmail.com>
2017-10-26 18:00:54 -0700

    tests working, reverse not yet functional

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 const LinkedList = require('./lib/linkedList');
 
 let ll = new LinkedList();
-
-ll.append('one');
-ll.append('two');
-ll.append('three');
-// ll.prepend('START');
-ll.remove(0);
+for(let i = 0; i < 5 ; i++){
+  ll.append(i);
+}
+console.log(ll);
 ll.print();
